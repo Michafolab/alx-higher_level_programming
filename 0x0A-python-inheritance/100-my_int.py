189f6bf7ca8a8c5123acb10c6943b8579ddc3fe2
Michafolab <topsonmich1@gmail.com>
2023-07-12 21:20:30 -0700

    a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+file: 100-my_int.py
+Class:
+-> MyInt
+"""
+
+
 class MyInt(int):
-    """ Class that inherits from class int"""
+    """ __eq__ and __ne__ inverted """
 
     def __eq__(self, other):
-        """ Method that returns != check """
-        return int.__ne__(self, other)
+        """ Inverted to not equal """
+        return super().__ne__(other)
 
     def __ne__(self, other):
-        """ Method that returns == check """
-        return int.__eq__(self, other)
+        """ Inverted to equal """
+        return super().__eq__(other)
