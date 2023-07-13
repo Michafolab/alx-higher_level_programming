6bcde2897ad0a20a6cc660e20ab0b655777fdb08
Michafolab <topsonmich1@gmail.com>
2023-07-12 21:00:36 -0700

    a class Rectangle that inherits from BaseGeometry (7-base_geometry.py…

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+"""
+file: 3-is_kind_of_class
+Class:
+-> is_kind_of_class
+"""
+
+
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
-
-    Args:
-        obj: object
-        a_class: class type
-
-    Returns:
-        True if obj is an instance of a_class
-        False, otherwise
-    """
+    """ Compare is obj is an instance of a_class """
     return isinstance(obj, a_class)
