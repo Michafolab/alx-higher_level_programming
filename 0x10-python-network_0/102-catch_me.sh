c24af53a63b29e6f8a6a016d78a946d82909f328
Michafolab <topsonmich1@gmail.com>
2023-09-28 18:23:09 -0700

    Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Script that makes a request to causes an specific response
+# Script that makes a request to causes a response
+
 curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "You got me!"
